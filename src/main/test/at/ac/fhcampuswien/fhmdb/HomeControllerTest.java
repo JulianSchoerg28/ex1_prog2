b06c569a0d2a6aa91fb630152dc226f65c2ffa66
Julian Schörg <julian.schoerg@stud.fh-campuswien.ac.at>
2024-02-23 10:53:44 +0100

    added first test test

--- src/main/test/at/ac/fhcampuswien/fhmdb/HomeControllerTest.java
+++ src/main/test/at/ac/fhcampuswien/fhmdb/HomeControllerTest.java
@@ -1,7 +1,13 @@
 package at.ac.fhcampuswien.fhmdb;
 
+import org.junit.jupiter.api.Test;
+
 import static org.junit.jupiter.api.Assertions.*;
 
 class HomeControllerTest {
-
+    @Test
+    public void testAddition() {
+        int result = 2 + 2;
+        assertEquals(4, result); // Überprüfe, ob das Ergebnis 4 ist
+    }
 }
