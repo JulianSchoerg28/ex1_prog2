2696d5bde7c44fa7293bd6160e1844f037ce0b9c
Julian Schörg <julian.schoerg@stud.fh-campuswien.ac.at>
2024-05-03 19:04:11 +0200

    fixed h2 dependency

--- src/main/java/module-info.java
+++ src/main/java/module-info.java
@@ -6,11 +6,13 @@ module at.ac.fhcampuswien.fhmdb {
     requires okhttp3;
     requires com.google.gson;
     requires ormlite.jdbc;
+    requires java.sql;
 
     opens at.ac.fhcampuswien.fhmdb to javafx.fxml;
     exports at.ac.fhcampuswien.fhmdb;
 
-    opens at.ac.fhcampuswien.fhmdb.models to com.google.gson; // Erlauben Sie Gson, auf die Movie-Klasse zuzugreifen
+    opens at.ac.fhcampuswien.fhmdb.models to com.google.gson;
+    opens at.ac.fhcampuswien.fhmdb.Database to com.google.gson, ormlite.jdbc; // Erlauben Sie Gson, auf die Movie-Klasse zuzugreifen
 
 }
 
